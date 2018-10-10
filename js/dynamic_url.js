00543fdc70e391c710b225d8c5b6abfd98b94578
jacobkap <jkkaplan6@gmail.com>
2018-10-10 08:14:37 -0400

    trying hash

--- js/dynamic_url.js
+++ js/dynamic_url.js
@@ -3,21 +3,27 @@ change_url = function() {
   agency_val = offense_agencies[$("#agency_dropdown").val()];
   crime_val = crime_values[$("#crime_dropdown").val()];
 
-  new_url =  "crime.html/state=" + state_val + "&agency=" + agency_val + "&crime=" + crime_val;
+  new_url =  "crime.html#state=" + state_val + "&agency=" + agency_val + "&crime=" + crime_val;
   window.history.pushState("", 'Crime', new_url);
   return(new_url);
 };
 
 
 change_data_from_url = function() {
-  new_url = window.location.pathname;
+  url = window.location.pathname;
 
-  split_url = new_url.split("&");
-  state_val =   split_url[0].replace("/state=", "");
-  state_val = _.indexOf(state_values, state_val);
+  split_url = url.split("&");
+  state_val = split_url[0].replace(/.*(?=state=)/, "");
+  state_val = state_val.replace("state=", "");
   agency_val = split_url[1].replace("agency=", "");
-  agency_val = _.indexOf(offense_agencies, agency_val);
   crime_val = split_url[2].replace("crime=", "");
+
+  state_val = state_val.replace(/%20/g, " ");
+  agency_val = agency_val.replace(/%20/g, " ");
+  crime_val = crime_val.replace(/%20/g, " ");
+
+  state_val = _.indexOf(state_values, state_val);
+  agency_val = _.indexOf(offense_agencies, agency_val);
   crime_val = _.indexOf(_.values(crime_values), crime_val);
   crime_val = _.keys(crime_values)[crime_val];
 
@@ -29,5 +35,6 @@ change_data_from_url = function() {
 
   $("#crime_dropdown").val(crime_val);
   $("#crime_dropdown").trigger("chosen:updated");
+  offenses_agencyChangeFun();
 
 };
