ecba1b9e84a59d16f2f8af0966f4c66e7f3432b2
jacobkap <jkkaplan6@gmail.com>
2018-10-11 08:06:50 -0400

    .

--- js/dynamic_url.js
+++ js/dynamic_url.js
@@ -1,16 +1,15 @@
-change_url = function(state_dropdown, agency_dropdown, category_dropdown, rate_checkbox,  agencies, category_values) {
-  state_val = state_values[$(state_dropdown).val()];
-  agency_val = agencies[$(agency_dropdown).val()];
+change_url = function(state_dropdown, agency_dropdown, category_dropdown, rate_checkbox, agencies, category_values) {
+  state_val    = state_values[$(state_dropdown).val()];
+  agency_val   = agencies[$(agency_dropdown).val()];
   category_val = category_values[$(category_dropdown).val()];
-
   rate_checked = $(rate_checkbox).prop("checked");
 
 
   new_url =  window.location.pathname +
-   "#state=" + state_val +
-   "&agency=" + agency_val +
-    "&category=" + category_val +
-    "&rate=" + rate_checked;
+   "#state="    + state_val +
+   "&agency="   + agency_val +
+   "&category=" + category_val +
+   "&rate="     + rate_checked;
   window.history.pushState("", 'Title', new_url);
   return(new_url);
 };
@@ -19,24 +18,24 @@ change_url = function(state_dropdown, agency_dropdown, category_dropdown, rate_c
 change_data_from_url = function(state_dropdown, agency_dropdown, category_dropdown, rate_checkbox, category_values, largest_agency, type) {
   url = window.location.hash;
 
-  split_url = url.split("&");
-  state_val = split_url[0].replace("#state=", "");
-  agency_val = split_url[1].replace("agency=", "");
+  split_url    = url.split("&");
+  state_val    = split_url[0].replace("#state=", "");
+  agency_val   = split_url[1].replace("agency=", "");
   category_val = split_url[2].replace("category=", "");
   rate_checked = split_url[3].replace("rate=", "");
 
-  state_val = state_val.replace(/%20/g, " ");
-  agency_val = agency_val.replace(/%20/g, " ");
+  state_val    = state_val.replace(/%20/g, " ");
+  agency_val   = agency_val.replace(/%20/g, " ");
   category_val = category_val.replace(/%20/g, " ");
 
-  state_val = _.indexOf(state_values, state_val);
+  state_val    = _.indexOf(state_values, state_val);
   category_val = _.indexOf(_.values(category_values), category_val);
   category_val = _.keys(category_values)[category_val];
 
   $(state_dropdown).val(state_val);
   $(state_dropdown).trigger("chosen:updated");
 
-  agencies = updateAgencies(type, largest_agency, agency_dropdown, state_dropdown);
+  agencies   = updateAgencies(type, largest_agency, agency_dropdown, state_dropdown);
   agency_val = _.indexOf(agencies, agency_val);
   $(agency_dropdown).val(agency_val);
   $(agency_dropdown).trigger("chosen:updated");
